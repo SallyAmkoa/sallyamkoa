6dd2ec52638b5f5b5c05b9397b71c2773c1457b7
Sally Amkoa <sallyamkoa@gmail.com>
2017-08-25 22:55:41 -0500

    corrected craps game code

--- projects/craps/craps.js
+++ projects/craps/craps.js
@@ -32,44 +32,44 @@ var diceRoll = function() {
 // }
 // })
 
-$('#roll-button').on('click', function(){
-    if(gameState.firstRoll) {
-      var die1 = diceRoll()
-      var die2 = diceRoll()
-      var sum = die1 + die2
-      $('.roll-result').text(sum)
-      if (sum ===2 ||sum ===3 ||sum ===12) {
-        $('.status').text('Sorry, you lose!').addClass('loser')
-        gameState.gameOver = true
-        $('#roll-button').attr('disabled', 'true')
-      } else if (sum===7 || sum ===11){
-        $('.status').text('Yay!!! You lucky bastard!').addClass('winner')
-        gameState.gameOver = true
-        $('#roll-button').attr('disabled', 'true')
-      } else {
-        $('.status').text('On point ' + sum).addClass('on-point')
-        gameState.pointNum = sum
-        gameState.firstRoll = false
-      }
-    }else {
+$('#roll-button').on('click', function() {
+  if(gameState.firstRoll) {
     var die1 = diceRoll()
     var die2 = diceRoll()
     var sum = die1 + die2
     $('.roll-result').text(sum)
-    if (sum===7) {
-      $('.status').text('Sorry, you lose!').addClass('loser')
-      gameState.gameOver =true
-        $('#roll-button').attr('disabled', 'true')
-      }else if (sum===gameState.pointNum){
-      $('.status').text('Yay!!! You lucky bastard!').addClass('winner')
-      gameState.gameOver =true
+    if(sum === 2 || sum === 3 || sum === 12) {
+      $('.status').text('You lose.').addClass('loser')
+      gameState.gameOver = true
       $('#roll-button').attr('disabled', 'true')
-    }else{
-      $('.status').text('Try again.')
+    } else if(sum === 7 || sum === 11) {
+      $('.status').text('You win!').addClass('winner')
+      gameState.gameOver = true
+      $('#roll-button').attr('disabled', 'true')
+    } else {
+      $('.status').text('On point ' + sum)
+      gameState.pointNum = sum
+      gameState.firstRoll = false
     }
+  } else {
+    var die1 = diceRoll()
+    var die2 = diceRoll()
+    var sum = die1 + die2
+    $('.roll-result').text(sum)
+    if(sum === 7) {
+      $('.status').text('You lose.').addClass('loser')
+      gameState.gameOver = true
+      $('#roll-button').attr('disabled', 'true')
+    } else if(sum === gameState.pointNum) {
+      $('.status').text('You win!').addClass('winner')
+      gameState.gameOver = true
+      $('#roll-button').attr('disabled', 'true')
+    } else {
+      $('.status').text('Try again')
     }
-  })
+  }
+})
 
-$('#play-again').on('click', function(){
+$('#play-again').on('click', function() {
   location.reload(true)
 })
