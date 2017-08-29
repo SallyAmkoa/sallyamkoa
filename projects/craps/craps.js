64923d2cf07913bd61c781c8ae0159c649034eb9
Sally Amkoa <sallyamkoa@gmail.com>
2017-08-28 21:00:52 -0500

    created refactored roll listener to multiple functions

--- projects/craps/craps.js
+++ projects/craps/craps.js
@@ -32,41 +32,56 @@ var diceRoll = function() {
 // }
 // })
 
-$('#roll-button').on('click', function() {
-  if(gameState.firstRoll) {
-    var die1 = diceRoll()
-    var die2 = diceRoll()
-    var sum = die1 + die2
-    $('.roll-result').text(sum)
+var rollDice =function(numberOfDice){
+  var sum =0
+  for (var i=0;i < numberOfDice; i++){
+    sum = sum+diceRoll()
+  }
+  return sum
+}
+var setGameOver = function(){
+  gameState.gameOver = true
+  $('#roll-button').attr('disabled', 'true')
+}
+
+var compareResult1 = function(sum){
     if(sum === 2 || sum === 3 || sum === 12) {
       $('.status').text('You lose.').addClass('loser')
-      gameState.gameOver = true
-      $('#roll-button').attr('disabled', 'true')
+    setGameOver()
     } else if(sum === 7 || sum === 11) {
       $('.status').text('You win!').addClass('winner')
-      gameState.gameOver = true
+    setGameOver()
       $('#roll-button').attr('disabled', 'true')
     } else {
       $('.status').text('On point ' + sum)
       gameState.pointNum = sum
       gameState.firstRoll = false
     }
+}
+
+var compareAdditionalResult = function(sum){
+  if(sum === 7) {
+    $('.status').text('You lose.').addClass('loser')
+    gameState.gameOver = true
+    $('#roll-button').attr('disabled', 'true')
+  } else if(sum === gameState.pointNum) {
+    $('.status').text('You win!').addClass('winner')
+    gameState.gameOver = true
+    $('#roll-button').attr('disabled', 'true')
   } else {
-    var die1 = diceRoll()
-    var die2 = diceRoll()
-    var sum = die1 + die2
-    $('.roll-result').text(sum)
-    if(sum === 7) {
-      $('.status').text('You lose.').addClass('loser')
-      gameState.gameOver = true
-      $('#roll-button').attr('disabled', 'true')
-    } else if(sum === gameState.pointNum) {
-      $('.status').text('You win!').addClass('winner')
-      gameState.gameOver = true
-      $('#roll-button').attr('disabled', 'true')
+    $('.status').text('Try again')
+  }
+}
+
+$('#roll-button').on('click', function() {
+  if(gameState.firstRoll) {
+      var sum = rollDice(2)
+      $('.roll-result').text(sum)
+      compareResult1(sum)
     } else {
-      $('.status').text('Try again')
-    }
+    var sum = rollDice(2)
+    $('.roll-result').text(sum)
+    compareAdditionalResult(sum)
   }
 })
 
